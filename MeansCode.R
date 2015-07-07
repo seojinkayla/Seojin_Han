04d0789fc5dcca0ae57d548d2e83e47acf1a626d
seojinkayla <seojinkayla@hanmail.net>
2015-07-07 11:21:19 +0900

    Add MeansCode.R

---
+++ MeansCode.R
@@ -0,0 +1,14 @@
+d=1000000
+die <- sample(6,d,replace=TRUE)
+hist(die)
+
+hist(runif(10000)*10,main="")
+means <- numeric(10000)
+for (i in 1:10000)
+(means[i] <- mean(runif(5)*10))
+hist(means,freq=FALSE)
+mean(means)
+sd(means)
+xv <- seq(0,10,0.1)
+yv <- dnorm(xv,mean=mean(means),sd=sd(means))
+lines(xv,yv)
